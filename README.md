e6eefef14e4c8e8faf33b98ff2cf8436cb150a1b
Herbert V <herbert.videla@tektonlabs.com>
2016-10-19 09:23:36 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+App create following the guide made by Cordova to create a SPA \n Source: git@github.com:HerbertVidela/CordovaTestSpa.git
---
+++ js/app.js
@@ -0,0 +1,42 @@
+// We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
+(function () {
+
+  /* ---------------------------------- Local Variables ---------------------------------- */
+  var service = new EmployeeService();
+  var homeTpl = Handlebars.compile($('#home-tpl').html());
+  var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
+  service.initialize().done(function () {
+    renderHomeView();
+  });
+
+  /* --------------------------------- Event Registration -------------------------------- */
+  
+  /*  $('.search-key').on('keyup', findByName);
+    $('.help-btn').on('click', function() {
+      alert("Employee Directory v3.4");
+    });*/
+
+  document.addEventListener('DOMContentLoaded', function() {
+    FastClick.attach(document.body); //eliminar los 300ms de carga de los alerts
+  }, false);
+
+  /* ---------------------------------- Local Functions ---------------------------------- */
+  function findByName() {
+    service.findByName($('.search-key').val()).done(function (employees) {
+      /*var l = employees.length;
+      var e;
+      $('.employee-list').empty();
+      for (var i = 0; i < l; i++) {
+        e = employees[i];
+        $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+      }*/
+      $(".content").html(employeeListTpl(employees));
+    });
+  };
+
+  function renderHomeView() {
+    $('body').html(homeTpl());
+    $('.search-key').on('keyup', findByName);
+  };
+
+}());
