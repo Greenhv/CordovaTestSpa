9ce686bb1de5469db95845df3e204ba1105a63f5
Herbert V <herbert.videla@tektonlabs.com>
2016-10-19 20:54:41 -0500

    new files

--- js/app.js
+++ js/app.js
@@ -1,55 +1,46 @@
 // We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
 (function () {
 
-  /* ---------------------------------- Local Variables ---------------------------------- */
-  var service = new EmployeeService();
-  HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
-  EmployeeListView.prototype.template = 
-              Handlebars.compile($("#employee-list-tpl").html());
-  EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
+    /* ---------------------------------- Local Variables ---------------------------------- */
+    HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
+    EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
+    EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
 
-  service.initialize().done(function () {
-    router.addRoute('', function() {
-        $('body').html(new HomeView(service).render().$el);
-    });
-
-    router.addRoute('employees/:id', function(id) {
-        service.findById(parseInt(id)).done(function(employee) {
-            $('body').html(new EmployeeView(employee).render().$el);
+    var service = new EmployeeService();
+    service.initialize().done(function () {
+        router.addRoute('', function() {
+            console.log('empty');
+            $('body').html(new HomeView(service).render().$el);
         });
+
+        router.addRoute('employees/:id', function(id) {
+            console.log('details');
+            service.findById(parseInt(id)).done(function(employee) {
+                $('body').html(new EmployeeView(employee).render().$el);
+            });
+        });
+
+        router.start();
     });
 
-    router.start();
-  });
+    /* --------------------------------- Event Registration -------------------------------- */
+    document.addEventListener('deviceready', function () {
+        StatusBar.overlaysWebView( false );
+        StatusBar.backgroundColorByHexString('#ffffff');
+        StatusBar.styleDefault();
+        FastClick.attach(document.body);
+        if (navigator.notification) { // Override default HTML alert with native dialog
+            window.alert = function (message) {
+                navigator.notification.alert(
+                    message,    // message
+                    null,       // callback
+                    "Workshop", // title
+                    'OK'        // buttonName
+                );
+            };
+        }
+    }, false);
 
-  /* --------------------------------- Event Registration -------------------------------- */
-  
-  /*  $('.search-key').on('keyup', findByName);
-    $('.help-btn').on('click', function() {
-      alert("Employee Directory v3.4");
-    });*/
-
-  document.addEventListener('DOMContentLoaded', function() {
-    FastClick.attach(document.body); //eliminar los 300ms de carga de los alerts
-  }, false);
-
-  /* ---------------------------------- Local Functions ---------------------------------- */
-  function findByName() {
-    service.findByName($('.search-key').val()).done(function (employees) {
-      /*var l = employees.length;
-      var e;
-      $('.employee-list').empty();
-      for (var i = 0; i < l; i++) {
-        e = employees[i];
-        $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-      }*/
-      $(".content").html(employeeListTpl(employees));
-    });
-  };
-
-  function renderHomeView() {
-    $('body').html(homeTpl());
-    $('.search-key').on('keyup', findByName);
-  };
+    /* ---------------------------------- Local Functions ---------------------------------- */
 
 }());
